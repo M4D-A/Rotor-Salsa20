b45a2a3c709e4b34e3d9289c750e7c30f2c9b275
M4D-A <jagielski.adamm@gmail.com>
2022-10-24 21:52:05 +0200

    beep bop

--- salsa_hash.v
+++ salsa_hash.v
@@ -8,20 +8,20 @@ module salsa_hash(
 
     input wire [31 : 0] data_in,
 
-    output reg [7 : 0] data_out
+    output wire [7 : 0] data_out
 );
 
 reg [7 : 0] state;
 reg [7 : 0] counter;
-reg [383 : 0] sipo_in;
 reg [511 : 0] data;
 reg [511 : 0] data_copy;
 
 wire [511 : 0] odd_out;
 wire [511 : 0] even_out;
 
-assign ready = (state == 0);
-assign writes = (state == 4);
+assign ready = (state == READY);
+assign writes = (state == WRITES);
+assign data_out = data[007 : 000];
 
 odd_round oddr(
     .data_in(data),
@@ -34,12 +34,8 @@ even_round evenr(
 );
 
 initial begin
-    data <= 512'b0;
-    data[031 : 000] <= 31'h61707865;
-    data[191 : 160] <= 31'h3320646e;
-    data[351 : 320] <= 31'h79622d32;
-    data[511 : 480] <= 31'h6b206574;
-    data_copy <= 512'h0;
+    data <= 512'bx;
+    data_copy <= 512'bx;
 
     counter <= 7'b0;
     state <= 7'b0;
@@ -49,13 +45,8 @@ always @(posedge clk) begin
     if (reset) begin
         state <= 7'b0;
         counter <= 7'b0;
-        
-        data <= 512'b0;
-        data[031 : 000] <= 31'h61707865;
-        data[191 : 160] <= 31'h3320646e;
-        data[351 : 320] <= 31'h79622d32;
-        data[511 : 480] <= 31'h6b206574;
-        data_copy <= 512'h0;
+        data <= 512'bx;
+        data_copy <= 512'bx;
     end 
     else begin
         case(state)
@@ -63,13 +54,13 @@ always @(posedge clk) begin
             if(start == 1) begin // wait for data and read key[0]
                 state <= 1;
                 counter <= 1;
-                sipo_in <= {data_in, sipo_in[383:32]};
+                data[383:000] <= {data_in, data[383:32]};
             end
         end
 
         1: begin
             counter <= counter + 1;
-            sipo_in <= {data_in, sipo_in[383:32]};
+            data[383:000] <= {data_in, data[383:32]};
             if (counter == 11) begin
                 counter <= 0;
                 state <= 2;
@@ -77,31 +68,36 @@ always @(posedge clk) begin
         end
 
         2: begin
-            data [159 : 032] <= sipo_in[127 : 000]; //key[3:0]
-            data [479 : 352] <= sipo_in[255 : 128]; //key[7:4]
-            data [255 : 192] <= sipo_in[309 : 256]; //nonce[1:0]
-            data [319 : 256] <= sipo_in[383 : 320]; //pos[1:0]
+            data[159 : 032] <= data[127 : 000]; //key[3:0]
+            data[479 : 352] <= data[255 : 128]; //key[7:4]
+            data[255 : 192] <= data[309 : 256]; //nonce[1:0]
+            data[319 : 256] <= data[383 : 320]; //pos[1:0]
+            data[031 : 000] <= 32'h61707865;
+            data[191 : 160] <= 32'h3320646e;
+            data[351 : 320] <= 32'h79622d32;
+            data[511 : 480] <= 32'h6b206574;
+
             sipo_in <= 384'bx;
             state <= 3;
         end
 
         3: begin
-            counter <= counter + 1;
-
             if(counter == 0) begin
                 data_copy <= data;
+                counter <= counter + 1;
             end
 
-            if( (counter > 0) && (counter < 21) ) begin
+            else if( (counter > 0) && (counter < 21) ) begin
                 if(counter % 2 == 0) begin
                     data <= even_out;
                 end
                 else begin
                     data <= odd_out;
                 end
+                counter <= counter + 1;
             end
 
-            if(counter == 21) begin
+            else if(counter == 21) begin
                 data[511:480] <= data[511:480] + data_copy[511:480];
                 data[479:448] <= data[479:448] + data_copy[479:448];
                 data[447:416] <= data[447:416] + data_copy[447:416];
@@ -118,33 +114,23 @@ always @(posedge clk) begin
                 data[095:064] <= data[095:064] + data_copy[095:064];
                 data[063:032] <= data[063:032] + data_copy[063:032];
                 data[031:000] <= data[031:000] + data_copy[031:000];
-            end
-
-            if (counter == 22) begin
-                data_out <= data[7 : 0];
-                data[503 : 000] <= data[511 : 008];
-                data[511 : 504] <= 8'b0;
                 state <= 4;
                 counter <= 0;
             end
         end
 
         4: begin
-            data_out <= data[7 : 0];
-            data[503 : 000] <= data[511 : 008];
-            data[511 : 504] <= 8'b0;
-            counter <= counter + 1;
             if (counter == 63) begin
-                data_out <= 8'bx;
-                data <= 512'b0;
-                data[031 : 000] <= 31'h61707865;
-                data[191 : 160] <= 31'h3320646e;
-                data[351 : 320] <= 31'h79622d32;
-                data[511 : 480] <= 31'h6b206574;
-
-                counter <= 0;
-                state <= 0;
-            end 
+                state <= 7'b0;
+                counter <= 7'b0;
+                data <= 512'bx;
+                data_copy <= 512'bx;
+            end
+            else begin
+                data[503 : 000] <= data[511 : 008];
+                data[511 : 504] <= 8'bx;
+                counter <= counter + 1;
+            end
         end
         endcase
     end
